4574f95d9bccac1a2a3dd9ef74d7398e2a97c5f0
koujidev <atabek122002@gmail.com>
2024-09-27 11:23:40 +0500

    Added Analytics and vercel.json

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import { App } from './App';
 
@@ -8,5 +9,6 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <Router>
     <App />
+    <Analytics />
   </Router>,
 );
